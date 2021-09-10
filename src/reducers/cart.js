7ad6344f64ac7011acea9e44cb0817e8791c42d3
zulwaran <81923601+zulwaran@users.noreply.github.com>
2021-09-10 07:03:07 +0700

    fix add item in cart

--- src/reducers/cart.js
+++ src/reducers/cart.js
@@ -17,7 +17,7 @@ const cart = (state = initialState, action) => {
     const user = firebase.auth().currentUser;
     switch (action.type) {
         case 'ADD_TO_CART':
-            axiosFirebase.post(`/cart/${state.cartId}/items/.json`)
+            axiosFirebase.post(`/cart/${state.cartId}/items/.json`, action.payload)
             return {
                 ...state,
                 userCart: [...state.userCart, action.payload],
